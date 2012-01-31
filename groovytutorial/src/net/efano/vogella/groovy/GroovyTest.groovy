3aec7792887b407111b2a8602ae00edcec378d8b
stefano casadei <scasadei@yahoo.com>
2012-01-31 10:43:53 +0100

    created vogella tutorials for groovy http://www.vogella.de/articles/Groovy/article.html

---
+++ groovytutorial/src/net/efano/vogella/groovy/GroovyTest.groovy
@@ -0,0 +1,11 @@
+package net.efano.vogella.groovy
+
+class GroovyTest {
+
+		
+		static void main(def args){
+			def mylist=[1,2,"Lars","4"]
+			mylist.each{ println it }
+		}
+
+}
---
+++ groovytutorial/src/net/efano/vogella/groovy/SumItUp.groovy
@@ -0,0 +1,12 @@
+package net.efano.vogella.groovy
+
+class SumItUp {
+	static sum(a,b){
+		a+b;
+		}
+	
+	static void main(args){
+		println sum(1,5)
+		println sum(1,2)
+		}
+}
